25258e81fbc3631a07c9238e9316b3622718502d
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-03 23:07:04 -0400

    adding soft assert

--- src/test/java/com/qa/DataProviderTest.java
+++ src/test/java/com/qa/DataProviderTest.java
@@ -3,6 +3,7 @@ package com.qa;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class DataProviderTest
 {
@@ -32,5 +33,19 @@ public class DataProviderTest
         Assert.assertTrue(true);
         Assert.assertEquals(1, 1/0);
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void softAssertTest()
+    {
+        // soft assert does the assertion at the end or whenever assertAll() is called
+        // even if there are exceptions in assertions (before assertAll()) then program will still continue
+        // and the result will produce on assertAll() call
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals("Hello", "Hello", "First soft assert failed");
+        softAssert.assertTrue("Hello".equals("hello"), "Second soft assert failed");
+        softAssert.assertTrue("Welcome".equals("welcomeeee"), "Third soft assert failed");
+        softAssert.assertAll();
     }
 }
