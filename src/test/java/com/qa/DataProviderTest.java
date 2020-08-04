f024e5afef8ad7d2fa42d336e6c7ec56bee0824b
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-04 11:40:51 -0400

    adding another xml file

--- src/test/java/com/qa/DataProviderTest.java
+++ src/test/java/com/qa/DataProviderTest.java
@@ -3,7 +3,6 @@ package com.qa;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 public class DataProviderTest
 {
@@ -31,21 +30,7 @@ public class DataProviderTest
         // hard asserts are just regular assert in TestNg. If any assert fails then the whole test fails. If any exceptions
         // are thrown and not handled then the whole program execution stops
         Assert.assertTrue(true);
-        Assert.assertEquals(1, 1/0);
+        Assert.assertEquals(1, 1);
         Assert.assertTrue(true);
-    }
-
-    @Test
-    public void softAssertTest()
-    {
-        // soft assert does the assertion at the end or whenever assertAll() is called
-        // even if there are exceptions in assertions (before assertAll()) then program will still continue
-        // and the result will produce on assertAll() call
-
-        SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals("Hello", "Hello", "First soft assert failed");
-        softAssert.assertTrue("Hello".equals("hello"), "Second soft assert failed");
-        softAssert.assertTrue("Welcome".equals("welcomeeee"), "Third soft assert failed");
-        softAssert.assertAll();
     }
 }
