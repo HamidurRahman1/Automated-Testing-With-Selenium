ae379677c02ebf9f08dba4b47d7eb32f5b649706
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-06 10:54:37 -0400

    adding drag and drop

---
+++ src/test/java/com/qa/tests/se/DragAndDropTest.java
@@ -0,0 +1,55 @@
+package com.qa.tests.se;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.logging.Logger;
+
+public class DragAndDropTest
+{
+    private static Logger logger;
+    private static WebDriver webDriver;
+
+    @BeforeClass
+    public void setup()
+    {
+        logger = Logger.getLogger(URLAndTitleCheckTest.class.getName());
+        webDriver = new ChromeDriver();
+    }
+
+    @Test
+    public void dropDownTest()
+    {
+        try
+        {
+            final String url = "https://www.testandquiz.com/selenium/testing.html";
+
+            webDriver.get(url);
+
+            WebElement from = webDriver.findElement(By.id("sourceImage"));
+            WebElement to = webDriver.findElement(By.id("targetDiv"));
+
+            new Actions(webDriver).dragAndDrop(from, to).build().perform();
+
+            Thread.sleep(5000);
+        }
+        catch(Exception e)
+        {
+            logger.severe("\t-> "+e.getMessage());
+        }
+    }
+
+    @AfterClass
+    public void tearDown()
+    {
+        webDriver.quit();
+        if(webDriver.toString().toLowerCase().contains("null"))
+            logger.info("\t-> web driver has successfully quit");
+    }
+}
