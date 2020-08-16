6a111cf425f0a7755ee4643be3e7a4cb2c567746
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-16 13:53:48 -0400

    init the cache look up

---
+++ src/test/java/com/qa/tests/se/CacheLookupWithPOM.java
@@ -0,0 +1,11 @@
+package com.qa.tests.se;
+
+/*
+    @CacheLookup will cache the the element(s) whenever first found and every subsequent call to will return the
+    cache version of it. Using this @ will save time finding element(s) by going through DOM. It is useful when with
+    static element(s) in nature.
+ */
+public class CacheLookupWithPOM
+{
+
+}
