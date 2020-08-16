71e6570de3262177b8b36483440705f7bf8cb2dd
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-16 14:02:05 -0400

    GitHubLoginPOM

---
+++ src/test/java/com/qa/tests/se/GitHubLoginPOM.java
@@ -0,0 +1,19 @@
+package com.qa.tests.se;
+
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+/*
+    To test out the @CacheLookup, I will use github login pages login field.
+    username will NOT be cached but password will be cached.
+ */
+public class GitHubLoginPOM
+{
+    @FindBy(how = How.ID, using = "login_field")
+    private String username;
+
+    @CacheLookup
+    @FindBy(how = How.ID, using = "password")
+    private String password;
+}
