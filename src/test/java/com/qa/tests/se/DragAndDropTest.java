5141a2a7e3c9ec2794a1a8e19c65998476dda958
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-18 12:06:07 -0400

    Base added to DragAndDrop

--- src/test/java/com/qa/tests/se/DragAndDropTest.java
+++ src/test/java/com/qa/tests/se/DragAndDropTest.java
@@ -1,28 +1,12 @@
 package com.qa.tests.se;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.logging.Logger;
-
-public class DragAndDropTest
+public class DragAndDropTest extends BaseBeforeClass
 {
-    private static Logger logger;
-    private static WebDriver webDriver;
-
-    @BeforeClass
-    public void setup()
-    {
-        logger = Logger.getLogger(URLAndTitleCheckTest.class.getName());
-        webDriver = new ChromeDriver();
-    }
-
     @Test
     public void dragAndDropTest()
     {
@@ -30,26 +14,18 @@ public class DragAndDropTest
         {
             final String url = "https://www.testandquiz.com/selenium/testing.html";
 
-            webDriver.get(url);
+            getWebDriver().get(url);
 
-            WebElement from = webDriver.findElement(By.id("sourceImage"));
-            WebElement to = webDriver.findElement(By.id("targetDiv"));
+            WebElement from = getWebDriver().findElement(By.id("sourceImage"));
+            WebElement to = getWebDriver().findElement(By.id("targetDiv"));
 
-            new Actions(webDriver).dragAndDrop(from, to).build().perform();
+            new Actions(getWebDriver()).dragAndDrop(from, to).build().perform();
 
             Thread.sleep(5000);
         }
         catch(Exception e)
         {
-            logger.severe("\t-> Error occurred: "+e.getMessage());
+            getLogger().severe("\t-> Error occurred: "+e.getMessage());
         }
-    }
-
-    @AfterClass
-    public void tearDown()
-    {
-        webDriver.quit();
-        if(webDriver.toString().toLowerCase().contains("null"))
-            logger.info("\t-> web driver has successfully quit");
     }
 }
