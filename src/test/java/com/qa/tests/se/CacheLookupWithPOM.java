6596cbc6f2696b89210808922214422fd5e03daf
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-18 12:03:51 -0400

    Base added to Cache

--- src/test/java/com/qa/tests/se/CacheLookupWithPOM.java
+++ src/test/java/com/qa/tests/se/CacheLookupWithPOM.java
@@ -1,39 +1,23 @@
 package com.qa.tests.se;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import java.util.logging.Logger;
 
 /*
     @CacheLookup will cache the the element(s) whenever first found and every subsequent call to will return the
     cache version of it. Using this @ will save time finding element(s) by going through DOM. It is useful when with
     static element(s) in nature.
  */
-public class CacheLookupWithPOM
+public class CacheLookupWithPOM extends BaseBeforeClass
 {
-    private static Logger logger;
-    private static WebDriver webDriver;
-
-    @BeforeClass
-    public void setup()
-    {
-        logger = Logger.getLogger(CacheLookupWithPOM.class.getName());
-        webDriver = new ChromeDriver();
-    }
-
     @Test
-    public void cacheLookupWithPOMtest()
+    public void cacheLookupWithPOMTest()
     {
         try
         {
-            webDriver.get("https://github.com/login");
+            getWebDriver().get("https://github.com/login");
 
-            GitHubLoginPOM pageObject = PageFactory.initElements(webDriver, GitHubLoginPOM.class);
+            GitHubLoginPOM pageObject = PageFactory.initElements(getWebDriver(), GitHubLoginPOM.class);
             pageObject.getUsername().sendKeys("myGithubUsername");
 
             long withoutCacheStartTime = System.currentTimeMillis();
@@ -51,15 +35,7 @@ public class CacheLookupWithPOM
         }
         catch(Exception e)
         {
-            logger.severe("\t-> Error occurred: "+e.getMessage());
+            getLogger().severe("\t-> Error occurred: "+e.getMessage());
         }
-    }
-
-    @AfterClass
-    public void tearDown()
-    {
-        webDriver.quit();
-        if(webDriver.toString().toLowerCase().contains("null"))
-            logger.info("\t-> web driver has successfully quit");
     }
 }
