af63335c378a32ee96052f323deedc16a47498b3
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-19 10:55:21 -0400

    addding Base to Nav

--- src/test/java/com/qa/tests/se/NavigationTest.java
+++ src/test/java/com/qa/tests/se/NavigationTest.java
@@ -1,57 +1,33 @@
 package com.qa.tests.se;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.logging.Logger;
-
-public class NavigationTest
+public class NavigationTest extends BaseBeforeClass
 {
-    private static Logger logger;
-    private static WebDriver webDriver;
-
-    @BeforeClass
-    public void setup()
-    {
-        logger = Logger.getLogger(NavigationTest.class.getName());
-        webDriver = new ChromeDriver();
-    }
-
     @Test
     public void navigationTest()
     {
         try
         {
             final String url = "https://github.com/";
-            webDriver.navigate().to(url);
+            getWebDriver().navigate().to(url);
             Thread.sleep(2000);
 
             final String url2 = "https://github.com/HamidurRahman1";
-            webDriver.navigate().to(url2);
+            getWebDriver().navigate().to(url2);
             Thread.sleep(2000);
 
-            webDriver.navigate().back();
+            getWebDriver().navigate().back();
             Thread.sleep(3000);
 
-            webDriver.navigate().forward();
+            getWebDriver().navigate().forward();
             Thread.sleep(3000);
-            Assert.assertEquals(webDriver.getCurrentUrl(), url2);
+            Assert.assertEquals(getWebDriver().getCurrentUrl(), url2);
         }
         catch (Exception e)
         {
-            logger.severe("\t-> Error occurred: "+e.getMessage());
+            getLogger().severe("\t-> Error occurred: "+e.getMessage());
         }
-    }
-
-    @AfterClass
-    public void tearDown()
-    {
-        webDriver.quit();
-        if(webDriver.toString().toLowerCase().contains("null"))
-            logger.info("\t-> web driver has successfully quit");
     }
 }
