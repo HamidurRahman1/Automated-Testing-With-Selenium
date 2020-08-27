dcf37eb2e858bbf1320a74af0aff18c9961eb3fa
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-27 17:26:18 -0400

    adding alertBox test

--- src/test/java/com/qa/tests/se/AlertsTest.java
+++ src/test/java/com/qa/tests/se/AlertsTest.java
@@ -1,15 +1,30 @@
 package com.qa.tests.se;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class AlertsTest extends BaseBeforeClass
 {
     @Test
-    public void cacheLookupWithPOMTest()
+    public void alertBoxTest()
     {
         try
         {
-            // tbi
+            final String url = "https://www.testandquiz.com/selenium/testing.html";
+            getWebDriver().get(url);
+            Thread.sleep(1000);
+
+            getWebDriver().findElement(By.xpath("/html/body/div/div[11]/div/p/button")).click();
+
+            Thread.sleep(1000);
+
+            Alert alert = getWebDriver().switchTo().alert();
+            getLogger().info("\t-> alert text: "+alert.getText());
+
+            Thread.sleep(1000);
+
+            alert.accept();
         }
         catch(Exception e)
         {
