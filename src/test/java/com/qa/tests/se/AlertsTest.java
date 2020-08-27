837d273eb7199933725fdebedb2cc86fff7879e0
Hamidur Rahman <Hamidur0405@gmail.com>
2020-08-26 22:57:38 -0400

    adding alert test

---
+++ src/test/java/com/qa/tests/se/AlertsTest.java
@@ -0,0 +1,19 @@
+package com.qa.tests.se;
+
+import org.testng.annotations.Test;
+
+public class AlertsTest extends BaseBeforeClass
+{
+    @Test
+    public void cacheLookupWithPOMTest()
+    {
+        try
+        {
+            // tbi
+        }
+        catch(Exception e)
+        {
+            getLogger().severe("\t-> Error occurred: "+e.getMessage());
+        }
+    }
+}
